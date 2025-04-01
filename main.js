c8e50a5ede9038240d2c0155b0b37353e9470841
Aaron Chee <108386419+realaaron@users.noreply.github.com>
2025-03-31 21:02:01 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",
+{
+    strings: ["Frontend-Developer", "Software Engineer", "Web Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true
+
+});
